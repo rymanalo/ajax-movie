2c5a1ea38757b685ae40a170091e25ad34281d04
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-05-02 01:51:23 -0700

    Add movie title, genre, plot descriptions.

--- movie.js
+++ movie.js
@@ -1,30 +1,48 @@
 $(document).ready(function(){
-
-
   $('form').keyup('click', function() {
     var form = $(this);
     var movie_title = $('input');
     var movie_input = $('input').val();
     var input = movie_input.replace(' ', '%20');
-
     $.ajax({
       url: 'http://www.omdbapi.com/?s=' + input,
       method: 'get',
       dataType: 'jsonp',
       success: function(movie) {
-
        var movies = movie['Search'];
        var availableTags = [];
         for(var i = 0; i < movies.length; i+=1) {
-
           availableTags.push(movies[i]['Title']);
-
         }
         $(function() {
           $( "#movieinput" ).autocomplete({
             source: availableTags
           });
         });
+      }
+    });
+  });
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    var form = $(this);
+    var movie_title = $('input');
+    var movie_input = $('input').val();
+    var input = movie_input.replace(' ', '%20');
+    $.ajax({
+      url: 'http://www.omdbapi.com/?t=' + input,
+      method: 'get',
+      dataType: 'jsonp',
+      success: function(movie) {
+        movie_title.val("");
+        movie_title.focus();
+        var title = movie['Title'];
+        var genre = movie['Genre'];
+        var plot = movie['Plot'];
+
+        $('.movies').append(title);
+        $('<li>' + genre + '</li>').insertAfter('.genre');
+        $('<li>' + plot + '</li>').insertAfter('.plot');
       }
     });
   });
